91723e1b98426a3a1c423badbcc86ebc6e208548
Mattias.Poppe <poppe.mattias@gmail.com>
2021-03-08 15:26:51 +0100

    DEV-1534 Disable network calls during testing
    Make sure that no network calls are made during testing.
    Add conftest.py and before every test patch out:
    
     - requests.head
     - paramiko.SSHClient.connect
    
    Results in an error if such a call is made. Patch them specifically
    with the desired functionality if you need it in a test.

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import paramiko
+import pytest
+import requests
+
+
+@pytest.fixture(autouse=True)
+def disable_network_calls(monkeypatch):
+    def stunted_head():
+        raise RuntimeError("Network access not allowed during testing!")
+
+    def stunted_ssh_connect():
+        raise RuntimeError("Network access not allowed during testing!")
+
+    monkeypatch.setattr(requests, "head", lambda *args, **kwargs: stunted_head())
+    monkeypatch.setattr(
+        paramiko.SSHClient, "connect", lambda *args, **kwargs: stunted_head()
+    )
